8eb7a03767778fc6546c3869bd12e33a5bc4f37b
Dan Savdie <savdie.d@gmail.com>
2018-09-01 15:53:00 +0200

    restart heroku test sendgrid

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -23,15 +23,16 @@ class PasswordResetsController < ApplicationController
   end
 
   def update
-    if params[:user][:password].empty?                  # Case (3)
+    if params[:user][:password].empty?
       @user.errors.add(:password, "can't be empty")
       render 'edit'
-    elsif @user.update_attributes(user_params)          # Case (4)
+    elsif @user.update_attributes(user_params)
       log_in @user
+      @user.update_attribute(:reset_digest, nil)
       flash[:success] = "Password has been reset."
       redirect_to @user
     else
-      render 'edit'                                     # Case (2)
+      render 'edit'
     end
   end
 

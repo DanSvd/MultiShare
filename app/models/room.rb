2bfce2468d33e6a602b0e96640e2fe2ea5bb8799
Dan Savdie <savdie.d@gmail.com>
2018-09-03 10:30:49 +0200

    app works correctly

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,6 @@
 class Room < ActiveRecord::Base
-  belongs_to :users
+  belongs_to :user, optional: true
   default_scope -> { order(created_at: :desc) }
-  validates :user_id, presence: true
   validates :name, presence: true, length: { maximum: 23 }
 
 end
